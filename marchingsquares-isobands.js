c477912067035ee399aa04a0656534569c51e48b
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-05-27 14:00:45 +0200

    Fix line coordinate issues in IsoContours implementation
    - Note, IsoContours does not necessarily return closed polygons. It
      rather returns paths that enclose an area with values lower than the
      threshold in clock-wise orientation.

--- marchingsquares-isobands.js
+++ marchingsquares-isobands.js
@@ -1,7 +1,7 @@
 /*!
 * @license GNU Affero General Public License.
 * Copyright (c) 2015, 2015 Ronny Lorenz <ronny@tbi.univie.ac.at>
-* v. 1.2.0
+* v. 1.2.1
 * https://github.com/RaumZeit/MarchingSquares.js
 */
 
