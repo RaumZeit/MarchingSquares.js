f3bfa36153c7155acc5643e590627454347dc5b8
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-06-06 22:26:33 +0200

    Bump to version 1.2.2

--- marchingsquares-isocontours.js
+++ marchingsquares-isocontours.js
@@ -1,7 +1,7 @@
 /*!
 * @license GNU Affero General Public License.
 * Copyright (c) 2015, 2015 Ronny Lorenz <ronny@tbi.univie.ac.at>
-* v. 1.2.1
+* v. 1.2.2
 * https://github.com/RaumZeit/MarchingSquares.js
 */
 
