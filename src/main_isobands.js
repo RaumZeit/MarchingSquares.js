ef1b7cda14c9d10cf35d5d6096d34c17ce7bb963
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-06-18 15:27:08 +0200

    Export quadTree() constructor as MarchingSquaresJS.prepareData() function
    - This enables users to pre-process their data, i.e. compute the
      QuadTree, and pass it to subsequent calls of `isoBands` or `isoLines`.
      Therefore, this reduces the overhead of computing the QuadTree for
      each call of the library functions.

--- src/main_isobands.js
+++ src/main_isobands.js
@@ -1,6 +1,8 @@
 
 import isoBands from './isobands.js';
+import { quadTree } from './quadtree.js';
 
 export {
   isoBands as isoBands,
+  quadTree as prepareData
 }
--- src/main_isolines.js
+++ src/main_isolines.js
@@ -1,7 +1,9 @@
 
 import isoLines from './isolines.js';
+import { quadTree } from './quadtree.js';
 
 export {
   isoLines as isoLines,
-  isoLines as isoContours
+  isoLines as isoContours,
+  quadTree as prepareData
 }
