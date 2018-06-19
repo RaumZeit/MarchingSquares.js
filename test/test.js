8c202055fea5d28317475dfc74b1da645ca0a5e5
Ronny Lorenz <ronny@tbi.univie.ac.at>
2018-06-19 16:50:03 +0200

    Adjust test suite to changes in implementation
    - Still not entirely sure why issue#6-3_5 was expecting outer frame
      before... It should be correct now, though.

--- test/test.js
+++ test/test.js
@@ -40,11 +40,11 @@ test('isoBands input validation', function (t) {
     var dataArr = [[1], [2], [3]];
 
     t.throws(function(){isoBands(null, 0, 5)}, /data is required/, 'missing data');
-    t.throws(function(){isoBands('string', 0, 5)} , /data should be an array of arrays/, 'invalid data');
-    t.throws(function(){isoBands([1], 0, 5)}, /data should be an array of arrays/, 'invalid data again');
-    t.throws(function(){isoBands(dataArr, null, 5)}, /lowerBand is required/, 'missing lowerBand');
+    t.throws(function(){isoBands('string', 0, 5)} , /array of arrays/, 'invalid data');
+    t.throws(function(){isoBands([1], 0, 5)}, /array of arrays/, 'invalid data again');
+    t.throws(function(){isoBands(dataArr, null, 5)}, /lowerBound is required/, 'missing lowerBand');
     t.throws(function(){isoBands(dataArr, 0, null)}, /bandWidth is required/, 'missing lowerBand');
-    t.throws(function(){isoBands(dataArr, 'number', 3)}, /lowerBand must be a number/, 'invalid lowerBand');
+    t.throws(function(){isoBands(dataArr, 'number', 3)}, /lowerBound must be a number/, 'invalid lowerBand');
     t.throws(function(){isoBands(dataArr, 23, 'string')}, /bandWidth must be a number/, 'invalid bandWidth');
     t.throws(function(){isoBands(dataArr, 23, 3, 'string')}, /options must be an object/, 'invalid options');
 
@@ -55,8 +55,8 @@ test('isoContours input validation', function (t) {
     var dataArr = [[1], [2], [3]];
 
     t.throws(function(){isoContours(null, 0)}, /data is required/, 'missing data');
-    t.throws(function(){isoContours('string', 0)}, /data should be an array of arrays/, 'invalid data');
-    t.throws(function(){isoContours([1], 0)}, /data should be an array of arrays/, 'invalid data again');
+    t.throws(function(){isoContours('string', 0)}, /array of arrays/, 'invalid data');
+    t.throws(function(){isoContours([1], 0)}, /array of arrays/, 'invalid data again');
     t.throws(function(){isoContours(dataArr, null)}, /threshold is required/, 'missing threshold');
     t.throws(function(){isoContours(dataArr, 'number')}, /threshold must be a number/, 'invalid threshold');
     t.throws(function(){isoContours(dataArr, 23, 'string')}, /options must be an object/, 'invalid options');
